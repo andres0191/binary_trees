9aa9df66d17dea6df19a9925930d0756e4c80f37
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-03-11 01:43:16 +0000

    [Solution - Task 1] has a left-child, the new node must take its place, and the old left-child must be set as the left-child of the new node.

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,17 +24,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
     new_node->left = NULL;
     new_node->right = NULL;
 
-    if (parent->right == NULL)
+    if (parent->left == NULL)
     {
-        parent->right = new_node;
+        parent->left = new_node;
         new_node->parent = parent;
     }
     else
     {
-        temp = parent->right;
-        parent->right = new_node;
+        temp = parent->left;
+        parent->left = new_node;
         new_node->parent = parent;
-        new_node->right = temp;
+        new_node->left = temp;
         temp->parent = new_node;
     }
     
