7915cfb942659faf49493ab401834331330ea176
andres0191 <andres_felipe0191@hotmail.com>
2020-03-12 20:27:36 +0000

    add task 12, function thata measure the size of a binary tree and 13, counts the node with at least 1 child in a binary tree

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,5 +7,9 @@
 **/
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	return (binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left) + 1);
 }
