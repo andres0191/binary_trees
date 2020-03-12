1fc3871cef62e18a1bd830099698c88ee2b29453
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-03-12 23:38:00 +0000

    [14-binary_tree_balance]  function that measures the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,29 @@
 #include "binary_trees.h"
 /**
+* height_balance - height of a binary tree balance
+* @tree:  is a  node of the tree to measure the balance factor
+* Return: height of the tree
+*/
+int height_balance(const binary_tree_t *tree)
+{
+int left_h = 0, right_h = 0;
+if (!tree)
+{
+return (0);
+}
+left_h = height_balance(tree->left);
+right_h = height_balance(tree->right);
+if (right_h < left_h)
+{
+return (left_h + 1);
+}
+else
+{
+return (right_h + 1);
+}
+}
+
+/**
 * binary_tree_balance - measures the balance factor of a binary tree
 * @tree :  is a  node of the tree to measure the balance factor
 *
@@ -7,5 +31,9 @@
 **/
 int binary_tree_balance(const binary_tree_t *tree)
 {
-
+if (!tree)
+{
+return (0);
+}
+return (height_balance(tree->left) - height_balance(tree->right));
 }
