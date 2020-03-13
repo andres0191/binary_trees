107b7d6bd2360ead503edebe2dd8c161fb25c592
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-03-13 00:33:26 +0000

    [18-binary_tree_uncle] function that finds the uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,11 +1,22 @@
 #include "binary_trees.h"
 /**
 * binary_tree_uncle - function that finds the uncle of a node
-* @node :  is a  node to find the uncle
+* @node:  is a  node to find the uncle
 *
 * Return: if tree is null, return 0
 **/
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	
+if (!node || !node->parent || !node->parent->parent)
+{
+return (NULL);
+}
+if (node->parent == node->parent->parent->left)
+{
+return (node->parent->parent->right);
+}
+else
+{
+return (node->parent->parent->left);
+}
 }
